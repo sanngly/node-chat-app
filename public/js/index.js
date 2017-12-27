ef3c66e16ac2c2068944727cd6a21e306c819e65
Sanjoy Ganguly -X (sanjgang - LARSEN Sanjgang TOUBRO INFOTECH LTD at Cisco) <sanjgang@cisco.com>
2017-12-27 16:40:14 +0530

    Added new server event and client event socket.io in both client and server

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io();
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('createEmail', {
+    to: 'reach.sanjoy.ganguly@gmail.com',
+    text: 'Hi, This is to inform you please stop making sound.'
+  });
+
+  socket.emit('createMessage', {
+    From: 'reach.sanjoy.ganguly@gmail.com',
+    text: 'That works for me.'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function (email) {
+  console.log('New email', email);
+});
+
+socket.on('newMessage', function (message) {
+  console.log('New message', message);
+});
--- server/server.js
+++ server/server.js
@@ -11,10 +11,25 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected.');
-
+  socket.emit('newEmail', {
+    from: 'reach.sanjoy.ganguly@gmail.com',
+    text: 'Hi, I am sending invitation for Zensar Interview',
+    createdAt: 68787
+  });
+  socket.emit('newMessage', {
+    from: 'Sardar',
+    text: 'Hi, Today it is holiday today',
+    createdAt: 123123
+  });
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+  });
   socket.on('disconnect', () => {
     console.log('User was disconnected from server');
   });
 });
 
-server.listen(3000, () => console.log(`node-chat app listening on port ${port}!. `));
+server.listen(3000, () => console.log(`node-chat app listening on port ${port}!.`));
