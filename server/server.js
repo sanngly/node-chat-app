51a820a4b18e6739b8c3a731638850f1f5ee4d20
Sanjoy Ganguly -X (sanjgang - LARSEN Sanjgang TOUBRO INFOTECH LTD at Cisco) <sanjgang@cisco.com>
2017-12-27 14:42:40 +0530

    Added socket.io in both client and server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var publicPath = path.join(__dirname, '../public');
-
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log(`node-chat app listening on port ${port}!. `));
+io.on('connection', (socket) => {
+  console.log('New user connected.');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected from server');
+  });
+});
+
+server.listen(3000, () => console.log(`node-chat app listening on port ${port}!. `));
