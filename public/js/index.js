0a8e349b07e4f25323cbae0bc727169c823443b5
sanngly <esanjoyganguly@gmail.com>
2018-01-01 17:28:13 +0530

    Add css for chat pages

--- public/js/index.js
+++ public/js/index.js
@@ -44,13 +44,14 @@ socket.emit('createMessage', {
   console.log('Got It Gotchha!.', data);
 }); 
 
+var messageTextBox = jQuery('[name="message"]');
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name="message"]').val()
+    text: messageTextBox.val()
   }, function () {
-    
+    messageTextBox.val('');
   });
 });
 
@@ -60,13 +61,16 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('geolocation not supported by your browser.');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
+    locationButton.removeAttr('disabled').text('Send Location');
+    //console.log(position);
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send Location');
     console.log('Unable to fetch the location');
   });
 });
