83a29678e651b5c5df64372ff400f7a577402c95
sanngly <esanjoyganguly@gmail.com>
2018-01-03 12:42:27 +0530

    Creating the users class and apis

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,34 +1,56 @@
-/* [{
-    id: '%$^&CVYVF&R^&',
-    name: 'Sanjeeva',
+[{
+    id: '/#12poiajdspfoif',
+    name: 'Andrew',
     room: 'The Office Fans'
-}]
-
-class Person {
-    constructor(name, age){
-        console.log(`Constructor called .. ${name} - ${age}`);
-        this.name = name;
-        this.age = age;
+  }]
+  
+  // addUser(id, name, room)
+  // removeUser(id)
+  // getUser(id)
+  // getUserList(room)
+  
+  class Users {
+    constructor () {
+      this.users = [];
     }
-    getPersonInfo() {
-        return `${this.name} and ${this.age}`;
+    addUser (id, name, room) {
+      var user = {id, name, room};
+      this.users.push(user);
+      return user;
     }
-}
-var me = new Person('Sanjoy', '38');
-//console.log(`${me.name} and ${me.age}`);
-console.log(me.getPersonInfo());
- */
-
- class Users {
-     constructor() {
-         this.users = [];
-     }
-
-     addUser(id, name, room) {
-         var user = {id, name, room};
-         this.users.push(user);
-         return user;
-     }
- }
-
- module.exports = {Users};
+    removeUser (id) {
+      var user = this.getUser(id);
+  
+      if (user) {
+        this.users = this.users.filter((user) => user.id !== id);
+      }
+  
+      return user;
+    }
+    getUser (id) {
+      return this.users.filter((user) => user.id === id)[0]
+    }
+    getUserList (room) {
+      var users = this.users.filter((user) => user.room === room);
+      var namesArray = users.map((user) => user.name);
+  
+      return namesArray;
+    }
+  }
+  
+  module.exports = {Users};
+  
+   // class Person {
+   //   constructor (name, age) {
+   //     this.name = name;
+   //     this.age = age;
+   //   }
+   //   getUserDescription () {
+   //     return `${this.name} is ${this.age} year(s) old.`;
+   //   }
+   // }
+   //
+   // var me = new Person('Andrew', 25);
+   // var description = me.getUserDescription();
+   // console.log(description);
+  
